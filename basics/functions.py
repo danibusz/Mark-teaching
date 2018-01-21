e9bdf07830d070737a625b3e41b7f3772c664532
danibusz <kovaridaniel1990@gmail.com>
2018-01-21 22:50:42 +0100

    My very first python file (not)

---
+++ basics/functions.py
@@ -0,0 +1,19 @@
+def print_funny():
+    print("Something funny")
+    print("Something even funnier")
+    print("The funniest thing")
+
+print_funny()
+
+def advanced_printing_basics(time, text):
+    for volume in range(0, time):
+        print(str(text))
+
+advanced_printing_basics(5, "It's alive!")
+
+
+def print_multiple_funny(time):
+    for volume in range(0, time):
+        print_funny()
+
+print_multiple_funny(7)
